21d1be9b473e7c74fb9cdcd4ad3d01fd76c1eb84
dot-dotdot <175088302+dot-dotdot@users.noreply.github.com>
2025-07-28 16:47:59 +0700

    Set round limit
    The game only last for 5 rounds. When a player or a computer reaches
    5 points, the event listener gets removed from buttons div and
    buttons are disabled.

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 "use strict";
 
 const CHOICES_COUNT = 3;
-
-function getComputerChoice() {
-    let choiceArray = ["rock", "paper", "scissors"];
-
-    return choiceArray[Math.floor(Math.random() * CHOICES_COUNT)];
-}
+const ROUNDS_TOTAL = 5;
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
-    const buttons = document.querySelector(".buttons");
+    const buttonsDiv = document.querySelector(".buttons");
 
-    buttons.addEventListener("click", (event) => {
+    buttonsDiv.addEventListener("click", function handleRound(event) {
         let roundResult = playRound(event.target.id, getComputerChoice());
         console.log(roundResult);
         console.log(humanScore, computerScore);
+
+        if (humanScore === ROUNDS_TOTAL || computerScore === ROUNDS_TOTAL) {
+            buttonsDiv.removeEventListener("click", handleRound);
+
+            const buttons = document.querySelectorAll(".buttons > button");
+            buttons.forEach(button => button.setAttribute("disabled", "true"));
+        }
     });
 
     // for (let i = 1; i <= 5; i++) {
@@ -47,6 +49,11 @@ function playGame() {
             computerScore++;
             return `You lose! ${computerChoice} beats ${humanChoice}`;
         }
+    }
+
+    function getComputerChoice() {
+        let choiceArray = ["rock", "paper", "scissors"];
+        return choiceArray[Math.floor(Math.random() * CHOICES_COUNT)];
     }
 }
 
